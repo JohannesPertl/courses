deb488ad750d7a18c86bfbfcd4ab4744eb554e01
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2019-11-13 21:45:08 +0100

    Reformat output of true and false to "Ja" and "Nein"

--- js/script.js
+++ js/script.js
@@ -1,74 +1,82 @@
 // Global variables
 var JSON_FILE = "https://raw.githubusercontent.com/MobileSoftwareDevelopment/web-application-development/dev/data/testData.json";
 
-window.addEventListener("load", function() {
-  readDataFromJson(JSON_FILE);
-  var btn = document.getElementById("btnPrint");
-  btn.addEventListener("click", printTableView);
+window.addEventListener("load", function () {
+    readDataFromJson(JSON_FILE);
+    var btn = document.getElementById("btnPrint");
+    btn.addEventListener("click", printTableView);
 });
 
 function readDataFromJson(path) {
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (this.readyState === 4) {
-      // if (this.readyState === 4 && this.status === 200) { //TODO: Fix bug
-      var jsonObject = JSON.parse(this.responseText);
-      buildTable(jsonObject);
-    }
-  };
-  request.open("GET", path, true);
-  request.send();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            var jsonObject = JSON.parse(this.responseText);
+            buildTable(jsonObject);
+        }
+    };
+    request.open("GET", path, true);
+    request.send();
 }
 
 function addHeaders(table, keys) {
-  var header = table.createTHead();
-  var row = header.insertRow();
-  for (var i = 0; i < keys.length; i++) {
-    var cell = row.appendChild(document.createElement("th"));
-    cell.appendChild(document.createTextNode(keys[i]));
-    cell.className = "table-header";
-    cell.setAttribute("scope", "col");
-  }
+    var header = table.createTHead();
+    var row = header.insertRow();
+    for (var i = 0; i < keys.length; i++) {
+        var cell = row.appendChild(document.createElement("th"));
+        cell.appendChild(document.createTextNode(keys[i]));
+        cell.className = "table-header";
+        cell.setAttribute("scope", "col");
+    }
 }
 
 function buildTable(jsonObject) {
-  let attributes = [
-    "Unitcode",
-    "Name",
-    "Typ",
-    "Modul",
-    "SWS",
-    "ECTS",
-    "Semester",
-    "Wahlpflicht",
-    "Studium",
-    "Lehrender"
-  ];
-  var table = document.createElement("table");
-  var tbody = table.appendChild(document.createElement("tbody"));
+    let attributes = [
+        "Unitcode",
+        "Name",
+        "Typ",
+        "Modul",
+        "SWS",
+        "ECTS",
+        "Semester",
+        "Wahlpflicht",
+        "Studium",
+        "Lehrender"
+    ];
+    var table = document.createElement("table");
+    var tbody = table.appendChild(document.createElement("tbody"));
 
-  for (var i = 0; i < jsonObject.length; i++) {
-    var course = jsonObject[i];
-    if (i === 0) {
-      addHeaders(table, Object.keys(course));
+    for (var i = 0; i < jsonObject.length; i++) {
+        var course = jsonObject[i];
+        if (i === 0) {
+            addHeaders(table, Object.keys(course));
+        }
+        var row = tbody.insertRow();
+        let counter = 0;
+        Object.keys(course).forEach(function (k) {
+            counter++;
+            console.log(k);
+            var cell = row.insertCell();
+            cell.appendChild(document.createTextNode(course[k]));
+            // Format Wahlpflichtfach
+            switch(cell.innerText.toLocaleLowerCase()){
+                case "true":
+                    cell.innerText = "Ja";
+                    break;
+                case "false":
+                    cell.innerText = "Nein";
+            }
+
+            if (counter === 0) {
+                cell.setAttribute("scope", "row");
+            }
+            cell.setAttribute("data-label", attributes[counter]);
+        });
     }
-    var row = tbody.insertRow();
-    let counter = 0;
-    Object.keys(course).forEach(function(k) {
-      counter++;
-      console.log(k);
-      var cell = row.insertCell();
-      cell.appendChild(document.createTextNode(course[k]));
-      if (counter === 0) {
-        cell.setAttribute("scope", "row");
-      }
-      cell.setAttribute("data-label", attributes[counter]);
-    });
-  }
-  document.getElementById("container").appendChild(table);
+    document.getElementById("container").appendChild(table);
 }
 
 
 function printTableView() {
-  window.print();
+    window.print();
 }
