653a0c7c8607c9930164dee22b55fc1674786325
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2019-11-15 16:11:11 +0100

    Implement Search function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
-// Global variables
-var JSON_FILE = "https://raw.githubusercontent.com/MobileSoftwareDevelopment/courses/dev/data/testData.json";
+// Global Variables
+const JSON_FILE = "https://raw.githubusercontent.com/MobileSoftwareDevelopment/courses/dev/data/testData.json";
 
 window.addEventListener("load", function () {
+
     readDataFromJson(JSON_FILE);
-    var btn = document.getElementById("btnPrint");
+    let btn = document.getElementById("btnPrint");
     btn.addEventListener("click", printTableView);
 });
 
+function SearchTable() {
+    let input = document.getElementById("search-courses").value.trim().toLowerCase();
+    let table = document.getElementById("table");
+    for (let row of table.rows) {
+        if (row.rowIndex === 0) continue;
+        let foundMatch = false;
+        for (let cell of row.cells) {
+            cell.style.backgroundColor = '#f8f8f8';
+            cell.style.color = '#000000';
+            if (input) {
+                if (cell.innerText.toLowerCase().includes(input)) {
+                    cell.style.backgroundColor = "rgba(168,168,168,0.2)";
+                    cell.style.color = '#cc0033';
+                    foundMatch = true;
+                }
+                row.style.display = foundMatch ? 'table-row' : 'none';
+            } else {
+                row.style.display = 'table-row';
+            }
+        }
+    }
+}
+
 function readDataFromJson(path) {
-    var request = new XMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            var jsonObject = JSON.parse(this.responseText);
+            let jsonObject = JSON.parse(this.responseText);
             buildTable(jsonObject);
         }
     };
@@ -20,10 +44,10 @@ function readDataFromJson(path) {
 }
 
 function addHeaders(table, keys) {
-    var header = table.createTHead();
-    var row = header.insertRow();
-    for (var i = 0; i < keys.length; i++) {
-        var cell = row.appendChild(document.createElement("th"));
+    let header = table.createTHead();
+    let row = header.insertRow();
+    for (let i = 0; i < keys.length; i++) {
+        let cell = row.appendChild(document.createElement("th"));
         cell.appendChild(document.createTextNode(keys[i]));
         cell.className = "table-header";
         cell.setAttribute("scope", "col");
@@ -43,23 +67,27 @@ function buildTable(jsonObject) {
         "Studium",
         "Lehrender"
     ];
-    var table = document.createElement("table");
-    var tbody = table.appendChild(document.createElement("tbody"));
+    let table = document.createElement("table");
+    table.id = "table";
+    let tbody = table.appendChild(document.createElement("tbody"));
 
-    for (var i = 0; i < jsonObject.length; i++) {
-        var course = jsonObject[i];
+    for (let i = 0; i < jsonObject.length; i++) {
+        let course = jsonObject[i];
         if (i === 0) {
             addHeaders(table, Object.keys(course));
         }
-        var row = tbody.insertRow();
+
+        let row = tbody.insertRow();
         let counter = 0;
-        Object.keys(course).forEach(function (k) {
+        for (const k of Object.keys(course)) {
             counter++;
-            console.log(k);
-            var cell = row.insertCell();
+
+            let cell = row.insertCell();
             cell.appendChild(document.createTextNode(course[k]));
+
+
             // Format Wahlpflichtfach
-            switch(cell.innerText.toLocaleLowerCase()){
+            switch (cell.innerText.toLowerCase()) {
                 case "true":
                     cell.innerText = "Ja";
                     break;
@@ -71,7 +99,8 @@ function buildTable(jsonObject) {
                 cell.setAttribute("scope", "row");
             }
             cell.setAttribute("data-label", attributes[counter]);
-        });
+        }
+
     }
     document.getElementById("container").appendChild(table);
 }
