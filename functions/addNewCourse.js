6131f441249f653284fec1a0cc8264527c3c5592
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2019-12-06 18:04:01 +0100

    Add basic functionality to addNewCourse

--- functions/addNewCourse.js
+++ functions/addNewCourse.js
@@ -1,6 +1,11 @@
-function addNewCourse(course) {
-  // TODO JOE
-  console.log("save: " + course.unitcode);
+const fs = require('fs');
+function addNewCourse(courses, course) {
+  console.log(course);
+  course.Wahlpflicht.replace("", "")
+  courses.push(course);
+  console.log("save: " + course.Name);
+  let data = JSON.stringify(courses);
+  fs.writeFileSync("./data/courses.json", data);
   // return true if everything is right
   return true;
 }
