b13571ebd7b03bf36a385fd778dda595fc05a295
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2019-12-06 17:58:12 +0100

    Fix regex

--- views/updateCourseView.js
+++ views/updateCourseView.js
@@ -54,15 +54,15 @@ function updateCourseView(courses) {
         <h1>${inputFormHeader}</h1>
         <form action="/save-new-course" method="POST" class="input-form">
             Unitcode:<br>
-            <input type="text" id="unitcode" name="unitcode" value="${course.Unitcode}" 
+            <input type="text" id="unitcode" name="Unitcode" value="${course.Unitcode}" 
             autofocus required pattern="${regexUnitcode}" title="Unitcode darf noch nicht existieren. Nur Großbuchstaben und Zahlen">
             <br>
             Name:<br>
-            <input type="text" id="name" name="name" value="${course.Name}" 
+            <input type="text" id="name" name="Name" value="${course.Name}" 
             required pattern="${regexName}" title="Name darf noch nicht existieren">
             <br>
             Typ:<br>
-            <input list="typ" name="typ" value="${course.Typ}" 
+            <input list="typ" name="Typ" value="${course.Typ}" 
             required pattern="[A-Za-z0-9]+" title="Nur Buchstaben und Zahlen">
             <datalist id="typ"> 
             <option value="iL" /> 
@@ -72,31 +72,31 @@ function updateCourseView(courses) {
         </datalist> 
             <br>
             Modul:<br>
-            <input type="text" id="modul" name="modul" value="${course.Modul}" 
-            required pattern="[A-Za-z0-9]+" title="Nur Buchstaben und Zahlen">   
+            <input type="text" id="modul" name="Modul" value="${course.Modul}" 
+            required pattern="(.|\\s)*\\S(.|\\s)*" title="Feld darf nicht leer sein">   
             <br>         
             SWS:<br>
-            <input type="number" id="sws" min="0" max="100" name="sws" value="${course.SWS}" 
+            <input type="number" id="sws" min="0" max="100" name="SWS" value="${course.SWS}" 
             required>
             <br>            
             ECTS:<br>
-            <input type="number" id="ects" min="0" max="30" name="ects" value="${course.ECTS}" 
+            <input type="number" id="ects" min="0" max="30" name="ECTS" value="${course.ECTS}" 
             required>
             <br>            
             Semester:<br>
-            <input type="number" id="semester" min="0" max="20" name="semester" value="${course.Semester}" 
+            <input type="number" id="semester" min="0" max="20" name="Semester" value="${course.Semester}" 
             required> 
             <br>           
             Wahlpflicht:<br>
-            <input type="checkbox" id="wahlpflicht" name="wahlpflicht" value="${course.Wahlpflicht}" >
+            <input type="checkbox" id="wahlpflicht" name="Wahlpflicht" value="${course.Wahlpflicht}" >
             <br>               
             Studium:<br>
-            <input type="text" id="studium" name="studium" value="${course.Studium}" 
-            required pattern="[a-zA-Z]+" title="Nur Buchstaben"> 
+            <input type="text" id="studium" name="Studium" value="${course.Studium}" 
+            required pattern="[A-Z]+" title="Nur Großbuchstaben"> 
             <br>            
             Lehrender:<br>
-            <input type="text" id="lehrender" name="lehrender" value="${course.Lehrender}" 
-            required pattern="[a-zA-Z-]+" title="Nur Buchstaben und -">
+            <input type="text" id="lehrender" name="Lehrender" value="${course.Lehrender}" 
+            required pattern="(.|\\s)*\\S(.|\\s)*" title="Feld darf nicht leer sein">
             <br>   
             <br>    
             <button type="submit">Speichern</button>     
