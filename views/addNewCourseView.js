b67d3c7b080fc8a5142c2c2511aedea2a94b479a
Liliana Olczak <liliana.olczak@edu.fh-joanneum.at>
2019-12-09 21:14:27 +0100

    Fix HTML Validator Errors

--- views/addNewCourseView.js
+++ views/addNewCourseView.js
@@ -53,77 +53,77 @@ function addNewCourseView(courses) {
     <div class = "container">
     <div class = "row"> 
         <div class = "col-4 col-s-12">
-        <form action="/save-new-course" method="POST" class="input-form">
-            <p>
-            Unitcode:
-            <input type="text" id="unitcode" name="Unitcode" value="${course.Unitcode}" 
-            autofocus required pattern="${regexUnitcode}" title="Unitcode darf noch nicht existieren. Nur Großbuchstaben und Zahlen">
-            </p>
-            <p>
-            Name:
-            <input type="text" id="name" name="Name" value="${course.Name}" 
-            required pattern="${regexName}" title="Name darf noch nicht existieren">
-            </p>
-            <p>
-            Typ:
-            <input list="typ" name="Typ" value="${course.Typ}" 
-            required pattern="[A-Za-z0-9]+" title="Nur Buchstaben und Zahlen">
-            <datalist id="typ"> 
-            <option value="iL" > 
-            <option value="Se" > 
-            <option value="Vo" > 
-            <option value="Praktikum" > 
-        </datalist> 
-            </p>
-            <p>
-            Modul:
-            <input type="text" id="modul" name="Modul" value="${course.Modul}" 
-            required pattern="(.|\\s)*\\S(.|\\s)*" title="Feld darf nicht leer sein">   
-            </p>         
-            SWS:
-            <input type="number" id="sws" min="0" max="100" name="SWS" value="${course.SWS}" 
-            required>
-            <p>            
-            ECTS:
-            <input type="number" id="ects" min="0" max="30" name="ECTS" value="${course.ECTS}" 
-            required>
-            </p> 
-            <p>          
-            Semester:
-            <input type="number" id="semester" min="0" max="20" name="Semester" value="${course.Semester}" 
-            required> 
-            </p>
-            <p>                                  
-            Studium:
-            <input type="text" id="studium" name="Studium" value="${course.Studium}" 
-            required pattern="[A-Z]+" title="Nur Großbuchstaben"> 
-            </p>           
-            <p>           
-            Lehrender:
-            <input type="text" id="lehrender" name="Lehrender" value="${course.Lehrender}" 
-            required pattern="(.|\\s)*\\S(.|\\s)*" title="Feld darf nicht leer sein">
-            </p>  
-            Wahlpflicht:
-            <input type="checkbox" id="wahlpflicht" name="Wahlpflicht" value="${course.Wahlpflicht}" >
-            <br>   
-            <br>    
+            <form action="/save-new-course" method="POST" class="input-form">
+                <p>
+                Unitcode:
+                    <input type="text" id="unitcode" name="Unitcode" value="${course.Unitcode}" 
+                    autofocus required pattern="${regexUnitcode}" title="Unitcode darf noch nicht existieren. Nur Großbuchstaben und Zahlen">
+                </p>
+                <p>
+                Name:
+                    <input type="text" id="name" name="Name" value="${course.Name}" 
+                    required pattern="${regexName}" title="Name darf noch nicht existieren">
+                </p>
+                <p>
+                Typ:
+                    <input list="typ" name="Typ" value="${course.Typ}" 
+                    required pattern="[A-Za-z0-9]+" title="Nur Buchstaben und Zahlen">
+                        <datalist id="typ"> 
+                            <option value="iL" > 
+                            <option value="Se" > 
+                            <option value="Vo" > 
+                            <option value="Praktikum" > 
+                        </datalist> 
+                </p>
+                <p>
+                Modul:
+                <input type="text" id="modul" name="Modul" value="${course.Modul}" 
+                required pattern="(.|\\s)*\\S(.|\\s)*" title="Feld darf nicht leer sein">   
+                </p>
+                <p>      
+                SWS:
+                <input type="number" id="sws" min="0" max="100" name="SWS" value="${course.SWS}" required>
+                </p>   
+                <p>            
+                ECTS:
+                <input type="number" id="ects" min="0" max="30" name="ECTS" value="${course.ECTS}" required>
+                </p> 
+                <p>          
+                Semester:
+                <input type="number" id="semester" min="0" max="20" name="Semester" value="${course.Semester}" required> 
+                </p>
+                <p>                                  
+                Studium:
+                <input type="text" id="studium" name="Studium" value="${course.Studium}" 
+                required pattern="[A-Z]+" title="Nur Großbuchstaben"> 
+                </p>           
+                <p>           
+                Lehrender:
+                <input type="text" id="lehrender" name="Lehrender" value="${course.Lehrender}" 
+                required pattern="(.|\\s)*\\S(.|\\s)*" title="Feld darf nicht leer sein">
+                </p>
+                <p>
+                Wahlpflicht:
+                <input type="checkbox" id="wahlpflicht" name="Wahlpflicht" value="${course.Wahlpflicht}" >
+                </p>
+               </form>
            
             
-            <div class="print">
-            <button id="btnCheck">
-             <img
+    <div class="print">
+        <button id="btnCheck">
+            <img
               class="image"
               src="resources/iconCheck.png"
               alt="Speichern"
             />
            <span>SPEICHERN</span>
-          </button>
-          </a>
-          </div>
-          <div class = "col-8"> </div>
+        </button>
+       
+    </div>
+   <div class = "col-8"> </div>
         </div>
               
-        </form></div>
+       </div>
         </div>
     </body>
 </html>`;
